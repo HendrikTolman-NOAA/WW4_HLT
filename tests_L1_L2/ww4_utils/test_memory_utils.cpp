7e7d50e564dfeb3f15bcf67791f32bfb10002fe1
Dr. Ir. Hendrik L. Tolman <Hendrik.Tolman@NOAA.gov>
2026-03-17 13:08:31 -0400

    Change date in test_memory_utils.cpp header
    Updated the date in the file header comment. (fixing erroneous update)

--- tests_L1_L2/ww4_utils/test_memory_utils.cpp
+++ tests_L1_L2/ww4_utils/test_memory_utils.cpp
@@ -11,7 +11,7 @@
  * Administration. WAVEWATCH IV (TM) and WW4 (TM) are trademarks of the National
  * Weather Service.
  * @author Aldgisl, Hendrik L. Tolman
- * @date 2026-03-11
+ * @date 2026-02-27
  */
 
 #include "ww4_utils/memory_utils.hpp"
