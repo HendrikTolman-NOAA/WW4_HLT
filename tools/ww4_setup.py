4c84d0511f107441764bbb15c855661b8ee72657
Dr. Ir. Hendrik L. Tolman <Hendrik.Tolman@NOAA.gov>
2026-03-23 15:01:42 -0400

    Update author date in ww4_setup.py
    The date was in the future :-), my bad!

--- tools/ww4_setup.py
+++ tools/ww4_setup.py
@@ -11,7 +11,7 @@
 @copyright © 2026 National Weather Service, National Oceanic and Atmospheric
                Administration. WAVEWATCH IV (TM) and WW4 (TM) are trademarks
                of the National Weather Service.
-@author Aldgisl, Hendrik Tolman (Initial, 2026-03-24)
+@author Aldgisl, Hendrik Tolman (Initial, 2026-03-23)
 """
 
 import shutil
