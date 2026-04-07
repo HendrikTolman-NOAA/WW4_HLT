6a46cfacebd3d5e38a47700e66d94a4439be5aff
Dr. Ir. Hendrik L. Tolman <Hendrik.Tolman@NOAA.gov>
2026-04-07 09:26:17 -0400

    manual edits of w4core_init

--- src/ww4_core/w4core_init.cpp
+++ src/ww4_core/w4core_init.cpp
@@ -12,7 +12,7 @@
  * @author Main Author(s): Aldgisl (AI Persona), Hendrik L. Tolman
  * @author Contributors: Jules (Agentic AI)
  * @date Initial, 2026-04-03
- * @date Last Update, 2026-04-06
+ * @date Last Update, 2026-04-07
  * @note The architectural design of this routine follows the structure of
  *       the multi-grid shell (ww3_multi.F90) in WAVEWATCH III.
  *       Original author of WW3 multi-grid shell: Hendrik L. Tolman.
@@ -35,40 +35,63 @@ std::ofstream logFile;
 
 void w4core_init(const ww4_utils::DateTime &startTime) {
   //
-  // Load configuration
+  // 0.  General initialization ----------------------------------------------
+  // 0.1 Load configuration from ww4_run_config.yml file
+  //
   globalRunConfig = ww4_utils::loadRunConfig("ww4_run_config.yml");
 
   //
-  // Initialize profiling
-  ww4_utils::TimeManagement::initializeProfiling();
-
+  // 0.2 Initial standard output if requested
   //
-  // Open log file if requested
-  if (globalRunConfig.produceLogFile) {
-    logFile.open("log.ww4");
-  }
-
-  //
-  // Initial standard output
   if (globalRunConfig.produceStdOut) {
-    ww4_utils::ww4_std_out::writeInitialOutput(std::cout, "Multi-grid shell");
+    //
+    // 0.2.1 Initial standard output
+    //
+    ww4_utils::ww4_std_out::writeInitialOutput(std::cout, "ww4_stand_alone");
+    //
+    // 0.2.2 Identify being in initialization routine
+    //
     std::cout << "          * Initialization (w4core_init) starting: "
               << ww4_utils::TimeManagement::toFormattedString(startTime)
               << std::endl;
+    //
+    // 0.2.3 Report out run time configuration
     //
     reportRunConfig(globalRunConfig, std::cout);
   }
 
   //
-  // Initial log file output
-  if (globalRunConfig.produceLogFile && logFile.is_open()) {
-    ww4_utils::ww4_logfile::writeInitialOutput(logFile, "Multi-grid shell");
+  // 0.3 Start log file if requested
+  //
+  if (globalRunConfig.produceLogFile) {
+    //
+    // 0.3.1 Open log file
+    //
+    logFile.open("log.ww4");
+    //
+    // 0.3.2 Initial log file output
+    //
+    ww4_utils::ww4_logfile::writeInitialOutput(logFile, "ww4_stand_alone");
+    //
+    // 0.3.3 Identify being in initialization routine
+    //
     logFile << "          * Initialization (w4core_init) starting: "
             << ww4_utils::TimeManagement::toFormattedString(startTime)
             << std::endl;
     //
-    reportRunConfig(globalRunConfig, logFile);
+    // 0.3.4 Report out run time configuration
+    //
+    reportRunConfig(globalRunConfig, logFile);  }
   }
+  
+  //
+  // 0.4 Initialize profiling
+  //
+  ww4_utils::TimeManagement::initializeProfiling();  
+
+  //
+  // 1.  XXXXXXXXX -----------------------------------------------------------
+  //
 }
 
 const ww4_utils::RunConfig &getRunConfig() { return globalRunConfig; }
