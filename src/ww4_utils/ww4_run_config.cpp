bc36bfbb1b1f97c160a647f2fda8f8b961e12c47
Dr. Ir. Hendrik L. Tolman <Hendrik.Tolman@NOAA.gov>
2026-04-16 11:05:27 -0400

    <FT-Main-code-elements-and-stand-alone-model>: [Tinkering with std and log output]

--- src/ww4_utils/ww4_run_config.cpp
+++ src/ww4_utils/ww4_run_config.cpp
@@ -110,9 +110,10 @@ void updateOutputConfig(OutputConfig &oc, std::string_view key_suffix,
  */
 void reportOutput(const OutputConfig &oc, std::string_view label,
                   std::ostream &os) {
-  os << "     " << label << " output : " << (oc.requested ? "yes" : "no")
-     << std::endl;
-  if (oc.requested) {
+  if (!oc.requested)
+    os << "     " << label << " not requested " << std::endl; 
+  } else {
+    os << "     " << label << " requested " << std::endl; 
     os << "        Interval        : " << oc.interval << " s" << std::endl;
     os << "        At first time   : " << (oc.atFirstTime ? "yes" : "no")
        << std::endl;
