da2493d8d64aa7d76f6477cd49ec54bffd743e28
Dr. Ir. Hendrik L. Tolman <Hendrik.Tolman@NOAA.gov>
2026-04-16 12:34:51 -0400

    <FT-Main-code-elements-and-stand-alone-model>: [Tinkering with std and log output]

--- src/ww4_core/w4core_init.cpp
+++ src/ww4_core/w4core_init.cpp
@@ -79,7 +79,7 @@ void w4core_init(const ww4_utils::DateTime &startTime,
       //
       // 0.4.2 Report out run start time
       //
-      os << "  Run starts at       : "
+      os << "  Run starts at "
          << ww4_utils::TimeManagement::toFormattedString(
                 ww4_utils::TimeManagement::getPresentDateTime())
          << std::endl;
@@ -112,7 +112,7 @@ void w4core_init(const ww4_utils::DateTime &startTime,
       //
       // 0.5.3 Report out run start time
       //
-      logFile << "  Run starts at       : "
+      logFile << "  Run starts at "
               << ww4_utils::TimeManagement::toFormattedString(
                      ww4_utils::TimeManagement::getPresentDateTime())
               << std::endl;
