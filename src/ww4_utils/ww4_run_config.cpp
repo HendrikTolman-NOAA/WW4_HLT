9e3ce033688ef1247f259049ff2efc95c5d265c4
Dr. Ir. Hendrik L. Tolman <Hendrik.Tolman@NOAA.gov>
2026-04-17 16:59:46 -0400

    <FT-Main-code-elements-and-stand-alone-model>: [Tinkering with std and log output]

--- src/ww4_utils/ww4_run_config.cpp
+++ src/ww4_utils/ww4_run_config.cpp
@@ -495,7 +495,7 @@ void reportRunConfig(const RunConfig &config, std::ostream &os) {
     }
   }
 
-  os << "     Time step              : " << config.timeStep << " s"
+  os << "     Time step            : " << config.timeStep << " s"
      << std::endl;
 
   os << "\n  Model input:" << std::endl;
