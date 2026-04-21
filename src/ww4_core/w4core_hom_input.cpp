426462727aa5adf1cdcdf0b9c4300fe8fe99b23b
Dr. Ir. Hendrik L. Tolman <Hendrik.Tolman@NOAA.gov>
2026-04-21 11:54:32 -0400

    Update file date information in w4core_hom_input.cpp

--- src/ww4_core/w4core_hom_input.cpp
+++ src/ww4_core/w4core_hom_input.cpp
@@ -11,7 +11,8 @@
  * Weather Service.
  * @author Main Author(s): Aldgisl (AI Persona), Hendrik L. Tolman
  * @author Contributors: Jules (Agentic AI)
- * @date 2026-04-21
+ * @date Initial 2026-04-17
+ * @date Last update 2026-04-21
  */
 
 #include "ww4_core/w4core_hom_input.hpp"
