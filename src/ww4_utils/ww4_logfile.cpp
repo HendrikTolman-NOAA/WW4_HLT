cfd00c463dc49b71aeb8872a8518c2bd514c3556
Dr. Ir. Hendrik L. Tolman <Hendrik.Tolman@NOAA.gov>
2026-04-23 09:50:54 -0400

    <FT-Main-code-elements-and-stand-alone-model>: [minor edits to log output]

--- src/ww4_utils/ww4_logfile.cpp
+++ src/ww4_utils/ww4_logfile.cpp
@@ -72,7 +72,7 @@ void writeLogTableHeader(std::ostream &os) {
         "------+\n"
      << "  |          Time           |       Inputs        |         Outputs   "
         "      |\n"
-     << "  |                         | WLV CUR WND ICE DPT | FLD PNT NST TRK "
+     << "  |      (end of step)      | WLV CUR WND ICE DPT | FLD PNT NST TRK "
         "RST API |\n"
      << "  +-------------------------+---------------------+-------------------"
         "------+"
